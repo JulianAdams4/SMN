ee22de92d9e3bdd9b8afda0c207619efdbda6aa1
mavemore94 <veronica940413@hotmail.com>
2017-05-24 17:41:07 -0500

    Sprint #1: #37 Crear modelo de datos de control.

--- app/models/datosControl.model.js
+++ app/models/datosControl.model.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
+
 var Schema = mongoose.Schema;
+var SchemaTypes = mongoose.Schema.Types;
 var datosControlSchema = new Schema({
   //aqui van los campos
+  nombreDato:{
+    type: String,
+    required: 'El nombre del dato es obligatorio.'
+  },
+  valorDato:{
+    type: SchemaTypes.Double,
+    required: 'El valor es obligatorio'
+  },
+  fechaDato: {
+    type: Date,
+    default: Date.now
+  },
+  unidadDato: {
+    type: String,
+    enum: ['%','mg/L','cm']
+  },
 });
 mongoose.model('DatosControl', datosControlSchema);
