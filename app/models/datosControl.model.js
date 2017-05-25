f7c361491fdf74b78628284bcdcc8cebcfba1bd9
joyce <jebenite@espol.edu.ec>
2017-05-24 22:13:30 -0500

    Sprint #1: #37 Crear modelo de datos de control- added idPaciente-fixed

--- app/models/datosControl.model.js
+++ app/models/datosControl.model.js
@@ -5,26 +5,30 @@ var Schema = mongoose.Schema;
 var SchemaTypes = mongoose.Schema.Types;
 var datosControlSchema = new Schema({
   //aqui van los campos
-  nombreDato:{
-    type: String,
-    required: 'El nombre del dato es obligatorio.'
-  },
-  valorDato:{
-    type: SchemaTypes.Double,
-    required: 'El valor es obligatorio'
-  },
-  fechaDato: {
-    type: Date,
-    default: Date.now
-  },
-  unidadDato: {
-    type: String,
-    enum: ['%','mg/L','cm']
-  },
   idPaciente: {
     type: Schema.Types.ObjectId,
     ref: 'Paciente',
     required: true
-  }
+  },
+  datos:[
+    {
+      nombreDato:{
+        type: String,
+        required: 'El nombre del dato es obligatorio.'
+      },
+      valorDato:{
+        type: SchemaTypes.Double,
+        required: 'El valor es obligatorio'
+      },
+      fechaDato: {
+        type: Date,
+        default: Date.now
+      },
+      unidadDato: {
+        type: String,
+        enum: ['%','mg/L','cm']
+      }
+    }
+  ]
 });
 mongoose.model('DatosControl', datosControlSchema);
