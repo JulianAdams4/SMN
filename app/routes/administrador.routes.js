5f8483881fbf82082cbd3f9d485698309c22ac25
Alfredleo7 <stalgonz@espol.edu.ec>
2017-06-15 22:57:22 -0500

    inicio de página pública principal

--- app/routes/administrador.routes.js
+++ app/routes/administrador.routes.js
@@ -5,4 +5,7 @@ var administrador = require('../controllers/administrador.controllers');
 module.exports = function(app) {
   app.route('/')
     .get(administrador.inicio);
+  app.route('/publico').get(function(req, res){
+    res.render('publico');
+  });
 };
