82deb810c24c418727417e133d0cfd58429bbf37
Alfredleo7 <stalgonz@espol.edu.ec>
2017-06-19 22:41:11 -0500

    diseño de login y controlador de login en front

--- app/routes/administrador.routes.js
+++ app/routes/administrador.routes.js
@@ -3,9 +3,15 @@
 var administrador = require('../controllers/administrador.controllers');
 
 module.exports = function(app) {
+
   app.route('/')
     .get(administrador.inicio);
+
   app.route('/publico').get(function(req, res){
     res.render('publico');
+  });
+
+  app.route('/login').get(function(req, res){
+      res.render('login');
   });
 };
