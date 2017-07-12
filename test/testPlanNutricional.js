5756aff9dec4b964699780625663bc24cf4045d2
Ivan Mosquera <Ivan Mosquera>
2017-07-11 20:21:30 -0500

    Pruebas de Get, Post y Put de Planes Nutricionales

--- test/testPlanNutricional.js
+++ test/testPlanNutricional.js
@@ -66,9 +66,129 @@ describe('/POST planNutricional', () => {
             });
         });
   });
+
+  //NO EXISTE PACIENTE
+  it('No existe un paciente con ese id para el plan nutricional', (done) => {
+    var datosPlan = {
+      idPaciente: '5928bafb9fa058092098ae88',
+      fechaDato: '2017-04-13',
+      documento: "http://talentoti.gov.co/635/articles-14312_empresas.pdf"
+    }
+    chai.request('http://localhost:3000')
+        .post('/api/planesNutricionalesPaciente/5928bafb9fa058092098ae88')
+        .send(datosPlan)
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+  });
+
+  //NO INGRESA FECHA DATO
+  it('No ingresa fecha de registro del Plan Nutricional', (done) => {
+    var datosPlan = {
+        idPaciente: patId,
+        documento: "http://talentoti.gov.co/635/articles-14312_empresas.pdf"
+    }
+    chai.request('http://localhost:3000')
+        .post('/api/planesNutricionales')
+        .send(datosPlan)
+        .end((err, res) => {
+          res.should.have.status(500);
+          done();
+        });
+  });
+
+  //NO INGRESA DOCUMENTO
+  it('No ingresa documento del Plan Nutricional', (done) => {
+    var datosPlan = {
+        idPaciente: patId,
+        fechaDato: '2017-04-13'
+    }
+    chai.request('http://localhost:3000')
+        .post('/api/planesNutricionales')
+        .send(datosPlan)
+        .end((err, res) => {
+          res.should.have.status(500);
+          done();
+        });
+  });
 });
 
+describe('/GET planNutricional', () => {
+      it('Debe obtener los planes nutricionales de un paciente', function(done){
+        chai.request('http://localhost:3000')
+            .get('/api/planesNutricionalesPaciente/'+patId)
+            .end(function(err, res){
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+              done();
+            });
+      });
+
+      it('No debe retornar ningún plan nutricional ya que no existe el paciente', function(done){
+        chai.request('http://localhost:3000')
+            .get('/api/planesNutricionalesPaciente/5928bafb9fa058092098ae88')
+            .end(function(err, res){
+                res.should.have.status(404);
+              done();
+            });
+      });
+  });
+
 describe('/PUT planNutricional vigente', () => {
+      it('Edita el documento de un plan nutricional con un determinado ID', function(done){
+        var datosPlan = new PlanNutricional({
+                idPaciente: patId,
+                fechaDato: '2017-04-13',
+                documento: "http://talentoti.gov.co/635/articles-14312_empresas.pdf"
+            });
+        datosPlan.save((err, plan) => {
+            chai.request('http://localhost:3000')
+                .put('/api/planesNutricionales/'+plan._id)
+                .send({ fechaDato: '2017-06-13',documento: "http://www.juntadeandalucia.es/empleo/recursos/material_didactico/especialidades/materialdidactico_econtabilidad_financiera/anexos/DOCUMENTOS%20MERCANTILES.pdf" })
+                .end(function(err, res){
+                  res.should.have.status(204);
+                  done();
+          });
+        });
+      });
+
+/*
+      it('No se ingresa el documento del plan nutricional al editar', function(done){
+        var datosPlan = new PlanNutricional({
+                idPaciente: patId,
+                fechaDato: '2017-04-13',
+                documento: "http://talentoti.gov.co/635/articles-14312_empresas.pdf"
+            });
+        datosPlan.save((err, plan) => {
+            chai.request('http://localhost:3000')
+                .put('/api/planesNutricionales/' + plan._id)
+                .send({ fechaDato: '2017-06-13' , documento:""})
+                .end(function(err, res){
+                  res.should.have.status(500);
+                  done();
+          });
+        });
+      });
+*/
+
+      it('No se ingresa fecha del plan nutricional al editar', function(done){
+        var datosPlan = new PlanNutricional({
+                idPaciente: patId,
+                fechaDato: '2017-04-13',
+                documento: "http://talentoti.gov.co/635/articles-14312_empresas.pdf"
+            });
+        datosPlan.save((err, plan) => {
+            chai.request('http://localhost:3000')
+                .put('/api/planesNutricionales/' + plan._id)
+                .send({ documento: "http://www.juntadeandalucia.es/empleo/recursos/material_didactico/especialidades/materialdidactico_econtabilidad_financiera/anexos/DOCUMENTOS%20MERCANTILES.pdf"})
+                .end(function(err, res){
+                  res.should.have.status(500);
+                  done();
+          });
+        });
+      });
+
       it('No actualiza a vigente porque ya lo es', function(done){
         var datosPlan = new PlanNutricional({
                 idPaciente: patId,
