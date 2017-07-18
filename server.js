b80f045d3f40c46c9044cdc6b0b488a1ba2904f0
jebenite <evelynbenitez263@gmail.com>
2017-07-17 20:00:29 -0500

    cambios menores mongoose.js

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ var express = require('./config/express');
 var db = mongoose();
 var app = express();
 
-app.listen(3000, function(){
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+app.listen(port, function(){
   console.log('Servidor corriendo en 3000');
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
