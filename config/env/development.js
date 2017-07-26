b7ea1b8c540d982ecacf915d6de2a412a6ab682d
jebenite <evelynbenitez263@gmail.com>
2017-07-26 14:03:47 -0500

    Sprint3: Tarea#202 a #206;#219 a #221

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-  db: 'mongodb://jebenite:jebenite@ds019028.mlab.com:19028/smn_api',
-  //db: 'mongodb://localhost/mean',
+  //db: 'mongodb://jebenite:jebenite@ds019028.mlab.com:19028/smn_api',
+  db: 'mongodb://localhost/mean',
   sessionSecret: 'ClaveDeSesiones',
   codeEncrypt: 'micodigodeencriptacion'
 };
