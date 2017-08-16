0df662ea01684f78619b85140f43d9b25fb974bd
mavemore <veronica940413@hotmail.com>
2017-08-16 16:05:04 -0500

    Cambios presentacion paciente

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //db: 'mongodb://mavemore:1234@ds155811.mlab.com:55811/softprueba',
-  db: 'mongodb://localhost/mean',
+  db: 'mongodb://mavemore:1234@ds155811.mlab.com:55811/softprueba',
+  //db: 'mongodb://localhost/mean',
   sessionSecret: 'ClaveDeSesiones',
   codeEncrypt: 'micodigodeencriptacion'
 };
