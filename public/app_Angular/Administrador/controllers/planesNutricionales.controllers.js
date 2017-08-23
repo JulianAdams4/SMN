3a3e72c2465a82e516bbf1de67f0d300dacfe54b
jebenite <evelynbenitez263@gmail.com>
2017-08-22 21:07:18 -0500

    correcciones fechas datos,planes

--- public/app_Angular/Administrador/controllers/planesNutricionales.controllers.js
+++ public/app_Angular/Administrador/controllers/planesNutricionales.controllers.js
@@ -248,17 +248,8 @@ angular.module('administrador').controller('PlanesController',['$scope','$http',
     }
 
     $scope.returnCurrentDate=function (){
-      var today = new Date();
-      var dd = today.getDate()+7;
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-      if(dd<10){
-        dd='0'+dd
-      }
-      if(mm<10){
-        mm='0'+mm
-      }
-      return today = yyyy+'-'+mm+'-'+dd;
+      var nextWeek = moment().add(8, 'days').format('YYYY-MM-DD');;
+      return nextWeek;
     }
 
   }]);
