ca73d8b50e578824c679e94f30480b3e543cb966
Alfredleo7 <stalgonz@espol.edu.ec>
2017-09-17 17:39:58 -0500

    cambios en descripción del paquete

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,6 @@
 module.exports = {
-  //db: 'mongodb://jebenite:jebenite@ds129053.mlab.com:29053/smnproduction',
-//  db: 'mongodb://mavemore:1234@ds155811.mlab.com:55811/softprueba',
-  db: 'mongodb://localhost/mean',
+  //db: 'mongodb://localhost/mean',
+  db: 'mongodb://jebenite:jebenite@ds129053.mlab.com:29053/smnproduction',
   sessionSecret: 'ClaveDeSesiones',
   codeEncrypt: 'micodigodeencriptacion'
 };
